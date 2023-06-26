dd4cbd6cf357057346736fca06975cdbf3bedc91
mehmetcandinc <dincmehmetcann@gmail.com>
2023-06-26 14:13:14 +0300

    Fixed(?) same img error

--- src/components/RestList.jsx
+++ src/components/RestList.jsx
@@ -15,20 +15,11 @@ const RestList = () => {
   useEffect(() => {
     fetchRests();
   }, []);
-  let image = document.createElement("img");
-  image.classList.add("card-img-top");
-  image.src = `${rests}?random=${Math.random()}`; // notice this
-  console.log(image.src);
+
   return (
     <div className="rests-list">
       {rests.map((rest, i) => (
-        <>
-          <SingleRest rest={rest} />
-          <img
-            src={"https://loremflickr.com/500/500/restaurant?random=1"} //ERROR SAME PHOTO PROMLEMS
-            alt="res"
-          />
-        </>
+        <SingleRest key={i} rest={rest} />
       ))}
     </div>
   );
--- src/components/SingleRest.jsx
+++ src/components/SingleRest.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const SingleRest = ({ rest }) => {
-  return <div className="single-rest"></div>;
+  const generateRandomImageUrl = () => {
+    const randomIndex = Math.floor(Math.random() * 1000);
+    const imageUrl = `https://loremflickr.com/500/500/restaurant?random=${randomIndex}`; // Double AltGr + , --> ``
+    return imageUrl;
+  };
+
+  const urlArray = [];
+  const randomImageUrl = generateRandomImageUrl();
+  urlArray.push(randomImageUrl);
+  console.log(randomImageUrl);
+
+  return (
+    <div key={rest.id} className="single-rest">
+      {urlArray.map((logo, i) => (
+        <img
+          key={i}
+          className="restaurants-logo"
+          src={logo}
+          alt="restaurant-logo"
+        />
+      ))}{" "}
+      <br></br>
+      <p>{rest.name}</p>
+    </div>
+  );
 };
 
 export default SingleRest;
